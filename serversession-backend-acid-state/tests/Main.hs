b446b0ea92f2cfe0999da71df4e9394e8ac336bd
Felipe Lessa <felipe.lessa@gmail.com>
2015-05-28 01:27:13 -0300

    Add storage tests for acid-state, both memory and local storage.

---
+++ serversession-backend-acid-state/tests/Main.hs
@@ -0,0 +1,21 @@
+module Main (main) where
+
+import Data.Acid.Local (openLocalState, createCheckpointAndClose)
+import Data.Acid.Memory (openMemoryState)
+import Test.Hspec
+import Web.ServerSession.Backend.Acid
+import Web.ServerSession.Core.StorageTests
+
+import qualified Control.Exception as E
+
+main :: IO ()
+main =
+  E.bracket
+    (AcidStorage <$> openLocalState emptyState)
+    (createCheckpointAndClose . acidState) $
+    \acidLocal -> hspec $ parallel $ do
+      acidMem <- runIO $ AcidStorage <$> openMemoryState emptyState
+      describe "AcidStorage on memory only" $
+        allStorageTests acidMem it runIO shouldBe shouldReturn shouldThrow
+      describe "AcidStorage on local storage" $
+        allStorageTests acidLocal it runIO shouldBe shouldReturn shouldThrow
