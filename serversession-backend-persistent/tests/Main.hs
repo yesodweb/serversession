4ff73d87d8835258d21ff584c88072727ee889e7
Felipe Lessa <felipe.lessa@gmail.com>
2015-05-31 16:30:44 -0300

    Travis: Use PostgreSQL 9.3.
    The persistent test is failing and I have no idea why, as it's not
    displaying any error message.  This commit changes a few things with
    PostgreSQL just to see if they make any difference.
    
    Note that the persistent test suite passes with flying colors both
    before and after this commit on my machine using PostgreSQL 9.4.2.

--- serversession-backend-persistent/tests/Main.hs
+++ serversession-backend-persistent/tests/Main.hs
@@ -19,7 +19,7 @@ P.mkMigrate "migrateAll" (serverSessionDefs (Proxy :: Proxy SessionMap))
 
 main :: IO ()
 main = hspec $
-  forM_ [ ("PostgreSQL", createPostgresqlPool "host=localhost user=test dbname=test password=test" 20)
+  forM_ [ ("PostgreSQL", createPostgresqlPool "user=test dbname=test password=test" 20)
         , ("SQLite",     createSqlitePool "test.db" 1) ] $
     \(rdbms, createPool) ->
   describe ("SqlStorage on " ++ rdbms) $ do
